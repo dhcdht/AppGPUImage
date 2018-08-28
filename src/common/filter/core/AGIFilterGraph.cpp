0786af7debedd9bfa8126aa86562d5bd22bf30d0
dhcdht <dhcdht@qq.com>
2018-08-28 23:01:42 +0800

    添加 AGIFilterGraph

---
+++ src/common/filter/core/AGIFilterGraph.cpp
@@ -0,0 +1,43 @@
+﻿//
+//  AGIFilterGraph.cpp
+//  AppGPUImage
+//
+//  Created by 董宏昌 on 2018/8/28.
+//  Copyright © 2018年 董宏昌. All rights reserved.
+//
+
+#include "AGIFilterGraph.h"
+
+
+AGIFilterGraph::AGIFilterGraph()
+{
+	
+}
+
+AGIFilterGraph::~AGIFilterGraph()
+{
+	
+}
+
+AGIPiplineInputPtr AGIFilterGraph::getInputAtIndex(int index)
+{
+	auto lock = this->lockGuardGraph();
+
+	auto sourcePtr = this->getSourceAtIndex(index);
+	auto input = dynamic_cast<AGIPiplineInputPtr::element_type*>(sourcePtr.get());
+	auto inputPtr = AGIPiplineInputPtr(input);
+
+	return inputPtr;
+}
+
+AGIPiplineOutputPtr AGIFilterGraph::getOutputAtIndex(int index)
+{
+	auto lock = this->lockGuardGraph();
+
+	auto targetPtr = this->getTargetAtIndex(index);
+	auto output = dynamic_cast<AGIPiplineOutputPtr::element_type*>(targetPtr.get());
+	auto outputPtr = AGIPiplineOutputPtr(output);
+
+	return outputPtr;
+}
+
---
+++ src/common/filter/core/AGIFilterGraph.h
@@ -0,0 +1,31 @@
+﻿//
+//  AGIFilterGraph.h
+//  AppGPUImage
+//
+//  Created by 董宏昌 on 2018/8/28.
+//  Copyright © 2018年 董宏昌. All rights reserved.
+//
+
+#ifndef AGIFilterGraph_h
+#define AGIFilterGraph_h
+
+#include "pipline/AGIPiplineGraph.hpp"
+#include "IO/AGIImage.h"
+#include "IO/input/AGIPiplineInput.h"
+#include "IO/output/AGIPiplineOutput.h"
+
+
+class AGIFilterGraph : public AGIPiplineGraph<AGIImagePtr, AGIImagePtr>
+{
+public:
+	AGIFilterGraph();
+	~AGIFilterGraph() override;
+
+public:
+	AGIPiplineInputPtr getInputAtIndex(int index);
+	AGIPiplineOutputPtr getOutputAtIndex(int index);
+};
+typedef std::shared_ptr<AGIFilterGraph> AgiFilterGraphPtr;
+
+
+#endif /* AGIFilterGraph_h */
