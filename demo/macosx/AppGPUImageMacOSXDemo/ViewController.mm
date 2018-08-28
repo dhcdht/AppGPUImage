c39a47cb116c740fbe3c73b2425e70be261441d5
董宏昌 <dhc@zhihu.com>
2018-08-28 18:10:52 +0800

    ios mac project

--- demo/macosx/AppGPUImageMacOSXDemo/ViewController.mm
+++ demo/macosx/AppGPUImageMacOSXDemo/ViewController.mm
@@ -54,85 +54,8 @@
 //    [self test_movie];
 
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"big_buck_bunny" withExtension:@"mp4"];
-    self.sdkTest->test_openMovieFile([[url absoluteString] cStringUsingEncoding:NSUTF8StringEncoding]);
-//    [self test_playMovieNextFrame];
+    const char *urlString = [[url absoluteString] cStringUsingEncoding:NSUTF8StringEncoding];
+    self.sdkTest->test_playerEngine(urlString);
 }
-
-//- (void)test_blend {
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"parallax-d" ofType:@"png"];
-//    const char *pathCStr = [path cStringUsingEncoding:NSUTF8StringEncoding];
-//
-//    NSString *path1 = [[NSBundle mainBundle] pathForResource:@"SplashScreen" ofType:@"png"];
-//    const char *pathCStr1 = [path1 cStringUsingEncoding:NSUTF8StringEncoding];
-//
-//    self.sdkTest->test_blend(pathCStr, pathCStr1);
-//
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [self test_blend];
-//    });
-//}
-//
-//- (void)setRepresentedObject:(id)representedObject {
-//    [super setRepresentedObject:representedObject];
-//
-//    // Update the view, if already loaded.
-//}
-//
-//- (void)test_movie {
-//    NSURL *url = [[NSBundle mainBundle] URLForResource:@"big_buck_bunny" withExtension:@"mp4"];
-//    self.asset = [AVAsset assetWithURL:url];
-//    NSError *error = nil;
-//    self.assetReader = [AVAssetReader assetReaderWithAsset:self.asset error:&error];
-//    AVAssetTrack *videoTrack = [[self.asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
-//
-//    NSMutableDictionary *outputSettings = [NSMutableDictionary dictionary];
-//    [outputSettings setObject:@(kCVPixelFormatType_32BGRA) forKey:(id)kCVPixelBufferPixelFormatTypeKey];
-//    AVAssetReaderTrackOutput *readerVideoTrackOutput = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:videoTrack outputSettings:outputSettings];
-//    [self.assetReader addOutput:readerVideoTrackOutput];
-//
-//    [self.assetReader startReading];
-//
-//    [self test_movie_next:readerVideoTrackOutput];
-//}
-//
-//- (void)test_movie_next:(AVAssetReaderTrackOutput*)readerVideoTrackOutput {
-//    if (self.assetReader.status == AVAssetReaderStatusReading) {
-//        CMSampleBufferRef sampleBuffer = [readerVideoTrackOutput copyNextSampleBuffer];
-//        CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
-//        CVPixelBufferLockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
-//
-//        void *data = CVPixelBufferGetBaseAddress(imageBuffer);
-//        size_t size = CVPixelBufferGetDataSize(imageBuffer);
-//
-//        if (data == nullptr && self.assetReader.status == AVAssetReaderStatusCompleted) {
-//            return;
-//        }
-//
-//        if (data != nullptr) {
-//            size_t inputSize = size-32;
-//            void *inputData = malloc(inputSize);
-//            memcpy(inputData, data, inputSize);
-//
-//            NSString *path1 = [[NSBundle mainBundle] pathForResource:@"SplashScreen" ofType:@"png"];
-//            const char *pathCStr1 = [path1 cStringUsingEncoding:NSUTF8StringEncoding];
-//            self.sdkTest->test_movie(inputData, (uint32_t)inputSize, pathCStr1);
-//
-//            free(inputData);
-//        }
-//        CVPixelBufferUnlockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
-//        CFRelease(sampleBuffer);
-//    }
-//
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1/60.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [self test_movie_next:readerVideoTrackOutput];
-//    });
-//}
-//
-//- (void)test_playMovieNextFrame {
-//    self.sdkTest->test_playMovieNextFrame();
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1/60.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [self test_playMovieNextFrame];
-//    });
-//}
 
 @end
